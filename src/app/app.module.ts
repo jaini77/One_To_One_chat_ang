a32c536c4b2aead026be2ddbfbea2369a218a773
= <=>
2023-07-03 14:52:36 +0530

    save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AuthModule } from './features/auth/auth.module';
 import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
+import {MatButtonModule} from '@angular/material/button';     
 import { ChatModule } from './features/chat/chat.module';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
